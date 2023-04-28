c4a7cd49879fd88887d6d070bc90df91c3c6462f
Michiel van der Meer <m.t.van.der.meer@liacs.leidenuniv.nl>
2023-04-28 10:32:55 +0200

    Update visualization code for Roadrunner

--- edugym/envs/roadrunner.py
+++ edugym/envs/roadrunner.py
@@ -1,11 +1,15 @@
-import numpy as np
-
 import gymnasium as gym
+import numpy as np
+import pygame
 from gymnasium import spaces
+
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
 
 
 class RoadrunnerEnv(gym.Env):
-    metadata = {"render_modes": ["terminal"]}
+    metadata = {"render_modes": ["terminal", "pygame"]}
 
     def __init__(self, render_mode=None, size=10, discrete=True):
         self.size = size  # The size of the single dimension grid
@@ -14,9 +18,10 @@ class RoadrunnerEnv(gym.Env):
         self.observation_space = spaces.Dict(
             {
                 "agent": spaces.Box(
-                            low=np.array([0,0]),
-                            high=np.array([self.size-1,self.size-1]),
-                            dtype=np.float32),
+                    low=np.array([0, 0], dtype=np.float64),
+                    high=np.array([self.size - 1, self.size - 1], dtype=np.float64),
+                    dtype=np.float64,
+                ),
             }
         )
 
@@ -36,6 +41,48 @@ class RoadrunnerEnv(gym.Env):
 
         assert render_mode is None or render_mode in self.metadata["render_modes"]
         self.render_mode = render_mode
+        if self.render_mode == "pygame":
+            self.init_pygame()
+
+    def init_pygame(self):
+        """
+        Initialize the pygame screen and font.
+        """
+        self.grid_size = 50
+        self.pg_width = self.size * self.grid_size
+        self.pg_height = self.grid_size * 3
+        self.screen = pygame.display.set_mode((self.pg_width, self.pg_height))
+        pygame.display.set_caption("RoadrunnerEnv")
+        pygame.init()
+        self.clock = pygame.time.Clock()
+        self.font = pygame.font.SysFont("Arial", 30)
+
+    def draw_grid(self):
+        """
+        Draw the game grid on then pygame screen.
+
+        The grid is a 1-D line with the agent, target and wall locations marked.
+        """
+        for i in range(self.size):
+            rect = pygame.Rect(i * self.grid_size, 0, self.grid_size, self.grid_size)
+            pygame.draw.rect(self.screen, WHITE, rect, 2)
+            if i == self._agent_location[0]:
+                pygame.draw.rect(self.screen, RED, rect)
+                text = self.font.render("A", True, BLACK)
+                text_rect = text.get_rect(center=rect.center)
+                self.screen.blit(text, text_rect)
+            elif i == self._target_location[0]:
+                pygame.draw.rect(self.screen, WHITE, rect)
+                text = self.font.render("T", True, BLACK)
+                text_rect = text.get_rect(center=rect.center)
+                self.screen.blit(text, text_rect)
+            elif i == self._wall_location[0]:
+                pygame.draw.rect(self.screen, WHITE, rect)
+                text = self.font.render("W", True, BLACK)
+                text_rect = text.get_rect(center=rect.center)
+                self.screen.blit(text, text_rect)
+            else:
+                pygame.draw.rect(self.screen, WHITE, rect)
 
     def _get_obs(self):
         return {"agent": self._agent_location}
@@ -44,25 +91,33 @@ class RoadrunnerEnv(gym.Env):
         return {"target": self._target_location, "wall": self._wall_location}
 
     def _render_frame(self):
-        for i in range(self.size):
-            if i == self._agent_location[0]:
-                print("=A=", end=" ")
-            elif i == self._target_location[0]:
-                print("=T=", end=" ")
-            elif i == self._wall_location[0]:
-                print("=W=", end=" ")
-            else:
-                print("= =", end=" ")
-        print()
+        if self.render_mode == "terminal":
+            for i in range(self.size):
+                if i == self._agent_location[0]:
+                    print("=A=", end=" ")
+                elif i == self._target_location[0]:
+                    print("=T=", end=" ")
+                elif i == self._wall_location[0]:
+                    print("=W=", end=" ")
+                else:
+                    print("= =", end=" ")
+            print()
+        elif self.render_mode == "pygame":
+            self.screen.fill(BLACK)
+            self.draw_grid()
+            pygame.display.update()
+            self.clock.tick(30)
+        else:
+            raise NotImplementedError
 
     def reset(self, seed=None, options=None):
         # We need the following line to seed self.np_random
         super().reset(seed=seed)
 
         # Choose the agent's location uniformly at random
-        self._agent_location = np.array([0, 0], dtype=np.float32)
-        self._target_location = np.array([self.size-2, 0], dtype=np.float32)
-        self._wall_location = np.array([self.size-1, 0], dtype=np.float32)
+        self._agent_location = np.array([0, 0], dtype=np.float64)
+        self._target_location = np.array([self.size - 2, 0], dtype=np.float64)
+        self._wall_location = np.array([self.size - 1, 0], dtype=np.float64)
 
         observation = self._get_obs()
         info = self._get_info()
@@ -84,10 +139,13 @@ class RoadrunnerEnv(gym.Env):
         new_dx = self._agent_location[1] + action[1]
 
         # Update the agent's location
-        self._agent_location = np.array([new_x, new_dx], dtype=np.float32)
+        self._agent_location = np.array([new_x, new_dx], dtype=np.float64)
 
         # An episode is done iff the agent has reached the target OR the agent has reached the wall
-        if self._agent_location[0] >= self._target_location[0] and self._agent_location[0] <= self._wall_location[0]:
+        if (
+            self._agent_location[0] >= self._target_location[0]
+            and self._agent_location[0] <= self._wall_location[0]
+        ):
             print("REACHED THE TARGET")
             terminated = True
             reward = 1
--- main.py
+++ main.py
@@ -1,17 +1,24 @@
 import gymnasium as gym
 import edugym
-env = gym.make("edugym/Roadrunner-v0", render_mode="terminal")
+import argparse
 
-import sys
-print(sys.path)
+def run_env(args):
+    env = gym.make(f"edugym/{args.env}", render_mode=args.render)
+    observation, info = env.reset(seed=42)
+    for i in range(2):
+        print(f"Step: {i}")
+        action = env.action_space.sample()
+        observation, reward, terminated, truncated, info = env.step(action)
+
+        if terminated or truncated:
+            observation, info = env.reset(seed=42)
+    env.close()
 
 
-observation, info = env.reset(seed=42)
-for i in range(2):
-    print(f"Step: {i}")
-    action = env.action_space.sample()
-    observation, reward, terminated, truncated, info = env.step(action)
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Test edugym")
+    parser.add_argument("--env", type=str, default="Roadrunner-v0")
+    parser.add_argument("--render", type=str, default="terminal")
+    args = parser.parse_args()
+    run_env(args)
 
-    if terminated or truncated:
-        observation, info = env.reset(seed=42)
-env.close()

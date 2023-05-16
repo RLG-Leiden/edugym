fc9495568479136d0e5350bb5bf09b03184529d6
andriusbern <andrius.bernatavicius@gmail.com>
2023-05-16 13:40:17 +0200

    final environment code

--- edugym/__init__.py
+++ edugym/__init__.py
@@ -17,3 +17,15 @@ register(
      entry_point="edugym.envs:SupermarketEnv",
      max_episode_steps=300,
 )
+
+register(
+    id="edugym/TrashBotContinuous-v0",
+    entry_point="edugym.envs:TrashBotContinuousEnv",
+    max_episode_steps=150,
+)
+
+register(
+    id="edugym/TrashBotDiscrete-v0",
+    entry_point="edugym.envs:TrashBotDiscreteEnv",
+    max_episode_steps=150,
+)

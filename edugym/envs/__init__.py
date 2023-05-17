2c4b4ac40b88551bed60e992ca0319b941d9a7c4
MMB2 <git@mmb2.click>
2023-05-17 09:47:14 +0000

    :recycle: properly register all envs

--- edugym/envs/__init__.py
+++ edugym/envs/__init__.py
@@ -1,2 +1,9 @@
+from edugym.envs.boulder import BoulderEnv
+from edugym.envs.catch import Catch
+from edugym.envs.golf import GolfEnv
+from edugym.envs.memorycorridor import MemoryCorridorEnv
 from edugym.envs.roadrunner import RoadrunnerEnv
+from edugym.envs.study import Study
 from edugym.envs.supermarket import SupermarketEnv
+from edugym.envs.tamagotchi import TamagotchiEnv
+from edugym.envs.trashbot import TrashBotDiscreteEnv, TrashBotContinuousEnv
